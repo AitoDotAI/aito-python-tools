2fb13f73ff0de71fb438ad25a01aae55146b1977
Tri Nguyen <tringuyenminh23@gmail.com>
2019-06-20 15:58:51 +0300

    use pd.api infer type instead of generate table schema for more stable dtype

--- converter/aito_converter.py
+++ converter/aito_converter.py
@@ -40,7 +40,7 @@ class AitoConverter:
             raise e
 
         if not input_file_name:
-            input_file_name = input_file_name.stem
+            input_file_name = input_file_path.stem
 
         if not output_file_name:
             output_file_name = input_file_name
@@ -58,34 +58,44 @@ class AitoConverter:
 
     @staticmethod
     def generate_aito_table_schema_from_pandas_df(table_df: pd.DataFrame,
-                                                  table_name: str,
-                                                  default_text_analyzer='Whitespace'):
+                                                  text_analyzer='Whitespace'):
         # TODO: This should not take the whole df but only take a sample
         """
         Return aito schema in dictionary format
         :param table_df: The pandas DataFrame containg table data
-        :param table_name: The table name
-        :param default_text_analyzer: default analyzer for text data.
-        It can be also be ['English', 'Finnish', 'Swedish, 'German']
-        :return: Aito Table Schema in json format
+        :param text_analyzer: analyzer for text data. It can be also be ['English', 'Finnish', 'Swedish, 'German']
+        :return: Aito Table Schema as dict
         """
         type_map = {'string': 'Text',
+                    'unicode': 'Text',
+                    'bytes': 'Text',
+                    'floating': 'Decimal',
                     'integer': 'Int',
-                    'number': 'Decimal',
+                    'mixed - integer': 'Decimal',
+                    'mixed - integer - float': 'Decimal',
                     'boolean': 'Boolean',
                     'decimal': 'Decimal',
-                    'datetime': 'String'}
-        pandas_schema = pd.io.json.build_table_schema(table_df, index=False)['fields']
+                    'datetime64': 'String',
+                    'datetime': 'String',
+                    'date': 'String',
+                    'timedelta64': 'String',
+                    'timedelta': 'String',
+                    'time': 'String',
+                    'period': 'String',
+                    'mixed': 'Text'
+                    }
         columns_schema = {}
-        for col in pandas_schema:
-            col_name = col['name']
-            columns_schema[col_name] = {'type': type_map[col['type']],
-                                        'nullable': True if table_df[col_name].isna().any().any() else False}
-            if columns_schema[col_name]['type'] == 'Text':
-                columns_schema[col_name]['analyzer'] = default_text_analyzer
+        for col in table_df.columns.values:
+            col_schema = {
+                'nullable': True if table_df[col].isna().any().any() else False,
+                'type': type_map[pd.api.types.infer_dtype(table_df[col].values, skipna=True)]
+            }
+            if col_schema['type'] == 'Text':
+                col_schema['analyzer'] = text_analyzer
+            columns_schema[col] = col_schema
 
-        table_schema = {table_name: {'type': 'table', 'columns': columns_schema}}
-        return json.dumps(table_schema)
+        table_schema = {'type': 'table', 'columns': columns_schema}
+        return table_schema
 
     def convert_file(self,
                      output_file_folder: Union[str, Path],
@@ -133,24 +143,27 @@ class AitoConverter:
         self.logger.info(f"Load file {str(input_file_path)} took {timeit.default_timer() - start}")
 
         start = timeit.default_timer()
-        convert_functions = {'csv': data.to_csv, 'xlsx': data.to_excel, 'json': data.to_json, 'ndjson': data.to_json}
 
         if special_fix_functions:
+            start = timeit.default_timer()
             for f in special_fix_functions:
                 data = f(data)
+            self.logger.info(f"Applying functions to dataframe took {timeit.default_timer() - start}")
 
         if not convert_options:
             convert_options = default_options[output_format]
         else:
             convert_options.update(default_options[output_format])
 
+        convert_functions = {'csv': data.to_csv, 'xlsx': data.to_excel, 'json': data.to_json, 'ndjson': data.to_json}
         output_file_path = output_file_folder / f"{output_file_name}.{output_format}"
         convert_functions[output_format](output_file_path, **convert_options)
         self.logger.info(f"Convert to {output_format} and write to file took {timeit.default_timer() - start}")
 
         if generate_aito_schema:
             start = timeit.default_timer()
-            schema = self.generate_aito_table_schema_from_pandas_df(data, output_file_name)
+            schema = self.generate_aito_table_schema_from_pandas_df(data)
+            print(schema)
             self.logger.info(f"Generate aito table schema took took {timeit.default_timer() - start}")
             with (output_file_folder / f"{output_file_name}_schema.json").open(mode='w') as f:
                 json.dump(schema, f, indent=4, sort_keys=True)
