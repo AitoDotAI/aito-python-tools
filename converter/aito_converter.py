08d1c55704af2dae02de278b187df5ecf38cb67c
Tri Nguyen <tringuyenminh23@gmail.com>
2019-08-02 16:10:37 +0300

    add function to read file, add default fixing option for datetime type

--- converter/aito_converter.py
+++ converter/aito_converter.py
@@ -2,59 +2,56 @@ import json
 import logging
 import timeit
 from pathlib import Path
-from typing import Union, List, Dict
+from typing import Union, List, Dict, Callable
 
 import pandas as pd
-
-from config import set_up_logger
 
 
 class AitoConverter:
     def __init__(self):
-        set_up_logger('converter')
         self.logger = logging.getLogger('AitoConverter')
         self.allowed_format = ['csv', 'json', 'xlsx', 'ndjson']
 
     def extract_and_validate_file_parameters(self,
-                                             output_file_folder: Union[str, Path],
-                                             input_file_path: Union[str, Path],
-                                             output_format: str,
-                                             input_format: str = None,
-                                             output_file_name: str = None,
-                                             input_file_name: str = None):
+                                             out_file_folder: Union[str, Path],
+                                             in_file_path: Union[str, Path],
+                                             out_format: str,
+                                             in_format: str = None,
+                                             out_file_name: str = None,
+                                             in_file_name: str = None):
         """
         Validate the file parameters of the converter
-        :param output_file_folder: path to output file folder
-        :param input_file_path: path to input file
-        :param output_format: file output format
-        :param input_format: if input format is not defined, it will be inferred from input file path suffix
-        :param output_file_name: if output file name is not defined, it will be the same as input file name
-        :param input_file_name: if input file name is not defined, it will be inferred from input file path stem
+        :param out_file_folder: path to output file folder
+        :param in_file_path: path to input file
+        :param out_format: file output format
+        :param in_format: if input format is not defined, it will be inferred from input file path suffix
+        :param out_file_name: if output file name is not defined, it will be the same as input file name
+        :param in_file_name: if input file name is not defined, it will be inferred from input file path stem
         :return: Extracted and validated
-                (output_file_folder, input_file_path, output_format, input_format, output_file_name. input_file_name)
+                (out_file_folder, in_file_path, out_format, in_format, out_file_name. in_file_name)
         """
         try:
-            input_file_path = Path(input_file_path)
-            output_file_folder = Path(output_file_folder)
+            in_file_path = Path(in_file_path)
+            out_file_folder = Path(out_file_folder)
         except Exception as e:
             raise e
 
-        if not input_file_name:
-            input_file_name = input_file_path.stem
+        if not in_file_name:
+            in_file_name = in_file_path.stem
 
-        if not output_file_name:
-            output_file_name = input_file_name
+        if not out_file_name:
+            out_file_name = in_file_name
 
-        if not input_format:
-            input_format = input_file_path.suffix.replace('.', '')
+        if not in_format:
+            in_format = in_file_path.suffix[1:]
 
-        if input_format not in self.allowed_format:
-            raise ValueError(f"Expect input format to be {str(self.allowed_format)} instead of {output_format}")
+        if in_format not in self.allowed_format:
+            raise ValueError(f"Expect input format to be {str(self.allowed_format)} instead of {out_format}")
 
-        if output_format not in self.allowed_format:
-            raise ValueError(f"Expect output format to be {str(self.allowed_format)} instead of {output_format}")
+        if out_format not in self.allowed_format:
+            raise ValueError(f"Expect output format to be {str(self.allowed_format)} instead of {out_format}")
 
-        return output_file_folder, input_file_path, output_format, input_format, output_file_name, input_file_name
+        return out_file_folder, in_file_path, out_format, in_format, out_file_name, in_file_name
 
     @staticmethod
     def generate_aito_table_schema_from_pandas_df(table_df: pd.DataFrame,
@@ -97,73 +94,115 @@ class AitoConverter:
         table_schema = {'type': 'table', 'columns': columns_schema}
         return table_schema
 
+    @staticmethod
+    def standard_fix(df: pd.DataFrame):
+        # Aito hasn't support datetime data yet. Converting all datetime data to string
+        for col in df:
+            if df[col].dtypes == 'datetime64[ns]':
+                df[col] = df[col].astype(str)
+        return df
+
+    def read_file(self, in_file_path: Path, in_format: str, load_options: Dict = None):
+        """
+        Load a file and return pandas Dataframe
+        :param in_file_path: path to input file
+        :param in_format: input format
+        :param load_options: dictionary contains arguments for pandas read function
+        :return:
+        """
+        start = timeit.default_timer()
+        read_functions = {'csv': pd.read_csv, 'xlsx': pd.read_excel, 'json': pd.read_json, 'ndjson': pd.read_json}
+        default_options = {'csv': {'low_memory': False}, 'xlsx': {}, 'json': {'orient': 'records'},
+                           'ndjson': {'orient': 'records', 'lines': True}}
+
+        options = default_options[in_format] if not load_options else load_options.update(default_options[in_format])
+        df = read_functions[in_format](in_file_path, **options)
+        self.logger.info(f"Read file {str(in_file_path)} took {timeit.default_timer() - start}")
+        return df
+
+    def apply_functions(self, df: pd.DataFrame, functions: List[Callable]):
+        """
+        Applying functions sequentially to a dataframe
+        :param df:
+        :param functions:
+        :return:
+        """
+        start = timeit.default_timer()
+        for f in functions:
+            start_f = timeit.default_timer()
+            df = f(df)
+            self.logger.info(f"Applying function {f.__name__} took {timeit.default_timer() - start_f}")
+        self.logger.info(f"Applying all functions took {timeit.default_timer() - start}")
+        return df
+
+    def to_format(self, df: pd.DataFrame, out_format: str, out_file_name: str, out_file_folder: Path,
+                  convert_options: Dict = None):
+        """
+
+        :param df:
+        :param out_format: file output format
+        :param out_file_name: if output file name is not defined, it will be the same as input file name
+        :param out_file_folder: path to output file folder
+        :param convert_options: dictionary contains arguments for pandas convert function
+        :return:
+        """
+        start = timeit.default_timer()
+        convert_functions = {'csv': df.to_csv, 'xlsx': df.to_excel, 'json': df.to_json, 'ndjson': df.to_json}
+        default_options = {'csv': {}, 'xlsx': {}, 'json': {'orient': 'records'},
+                           'ndjson': {'orient': 'records', 'lines': True}}
+        options = default_options[out_format] if not convert_options else \
+            convert_options.update(default_options[out_format])
+
+        out_file_path = out_file_folder / f"{out_file_name}.{out_format}"
+        convert_functions[out_format](out_file_path, **options)
+        self.logger.info(f"Convert to {out_format} and write to file took {timeit.default_timer() - start}")
+
     def convert_file(self,
-                     output_file_folder: Union[str, Path],
-                     input_file_path: Union[str, Path],
-                     output_format: str,
-                     input_format: str = None,
-                     output_file_name: str = None,
-                     input_file_name: str = None,
+                     out_file_folder: Union[str, Path],
+                     in_file_path: Union[str, Path],
+                     out_format: str,
+                     in_format: str = None,
+                     out_file_name: str = None,
+                     in_file_name: str = None,
                      load_options: Dict = None,
-                     special_fix_functions: List = None,
+                     special_fix_functions: List[Callable] = None,
                      convert_options: Dict = None,
                      generate_aito_schema: bool = True):
         """
         Converting a file into expected format and generate aito schema if required
-        :param output_file_folder: path to output file folder
-        :param input_file_path: path to input file
-        :param output_format: file output format
-        :param input_format: if input format is not defined, it will be inferred from input file path suffix
-        :param output_file_name: if output file name is not defined, it will be the same as input file name
-        :param input_file_name: if input file name is not defined, it will be inferred from input file path stem
+        :param out_file_folder: path to output file folder
+        :param in_file_path: path to input file
+        :param out_format: file output format
+        :param in_format: if input format is not defined, it will be inferred from input file path suffix
+        :param out_file_name: if output file name is not defined, it will be the same as input file name
+        :param in_file_name: if input file name is not defined, it will be inferred from input file path stem
         :param load_options: dictionary contains arguments for pandas read function
         :param convert_options: dictionary contains arguments for pandas convert function
-        :param special_fix_functions: List of partial functions that will be chained applied to the loaded pd.DataFrame.
+        :param special_fix_functions: List of partial functions that will be chained applied to the loaded pd.DataFrame
         :param generate_aito_schema: option to auto generate aito schema
         :return:
         """
-        start = timeit.default_timer()
         try:
-            output_file_folder, input_file_path, output_format, input_format, output_file_name, input_file_name = \
-                self.extract_and_validate_file_parameters(output_file_folder, input_file_path, output_format,
-                                                          input_format, output_file_name, input_file_name)
+            out_file_folder, in_file_path, out_format, in_format, out_file_name, in_file_name = \
+                self.extract_and_validate_file_parameters(out_file_folder, in_file_path, out_format,
+                                                          in_format, out_file_name, in_file_name)
         except Exception as e:
             raise e
 
-        read_functions = {'csv': pd.read_csv, 'xlsx': pd.read_excel, 'json': pd.read_json, 'ndjson': pd.read_json}
-        default_options = {'csv': {}, 'xlsx': {}, 'json': {'orient': 'records'},
-                           'ndjson': {'orient': 'records', 'lines': True}}
-
-        if not load_options:
-            load_options = default_options[input_format]
-        else:
-            load_options.update(default_options[input_format])
-
-        data = read_functions[input_format](input_file_path, **load_options)
-        self.logger.info(f"Load file {str(input_file_path)} took {timeit.default_timer() - start}")
-
-        start = timeit.default_timer()
+        df = self.read_file(in_file_path, in_format, load_options)
 
         if special_fix_functions:
-            start = timeit.default_timer()
-            for f in special_fix_functions:
-                data = f(data)
-            self.logger.info(f"Applying functions to dataframe took {timeit.default_timer() - start}")
-
-        if not convert_options:
-            convert_options = default_options[output_format]
+            special_fix_functions = [self.standard_fix] + special_fix_functions
         else:
-            convert_options.update(default_options[output_format])
+            special_fix_functions = [self.standard_fix]
+        df = self.apply_functions(df, special_fix_functions)
 
-        convert_functions = {'csv': data.to_csv, 'xlsx': data.to_excel, 'json': data.to_json, 'ndjson': data.to_json}
-        output_file_path = output_file_folder / f"{output_file_name}.{output_format}"
-        convert_functions[output_format](output_file_path, **convert_options)
-        self.logger.info(f"Convert to {output_format} and write to file took {timeit.default_timer() - start}")
+        self.to_format(df, out_format, out_file_name, out_file_folder, convert_options)
 
         if generate_aito_schema:
             start = timeit.default_timer()
-            schema = self.generate_aito_table_schema_from_pandas_df(data)
-            print(schema)
+            schema = self.generate_aito_table_schema_from_pandas_df(df)
             self.logger.info(f"Generate aito table schema took took {timeit.default_timer() - start}")
-            with (output_file_folder / f"{output_file_name}_schema.json").open(mode='w') as f:
+            with (out_file_folder / f"{out_file_name}_schema.json").open(mode='w') as f:
                 json.dump(schema, f, indent=4, sort_keys=True)
+
