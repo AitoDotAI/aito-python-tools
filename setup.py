9699df97ca4e0a3f6b781fcc5326fcca47335114
Tri Nguyen <tringuyenminh23@gmail.com>
2019-10-11 16:55:35 +0300

    use pandoc for long description, bump to 0.0.3

--- setup.py
+++ setup.py
@@ -1,24 +1,36 @@
 from setuptools import setup, find_packages
+import os
 
-install_requires = []
+install_requires = [
+    'python-dotenv',
+    'requests',
+    'aiohttp',
+    'pandas',
+    'ndjson',
+    'xlrd',
+    'langdetect'
+]
+
+if os.environ.get('CONVERT_README'):
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+else:
+    long_description = ''
+
+print('LONG DESCRIPTION', long_description)
+
+VERSION = '0.0.3'
 
 setup(
     name='aitoai',
-    version='0.0.1',
+    version=VERSION,
     author='aito.ai',
-    author_email='support@aito.ai',
+    author_email='admin@aito.ai',
     description='A collection of python support tools and scripts for Aito.ai',
+    long_description=long_description,
     url='https://github.com/AitoDotAI/aito-python-tools',
     packages=find_packages(exclude=['tests', 'tests.*']),
-    install_requires=[
-        'python-dotenv',
-        'requests',
-        'aiohttp',
-        'pandas',
-        'ndjson',
-        'xlrd',
-        'langdetect'
-    ],
+    install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
