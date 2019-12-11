69be754cfdfedd5a3f32084ef0462827abe1fe5f
Tri Nguyen <tringuyenminh23@gmail.com>
2019-12-11 17:51:27 +0200

    fix circle ci test MySQL, add separate MySQL unittests

--- tests/io/in/sample_invoice/create_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE invoice(
-  id serial primary key,
-  name VARCHAR(355) not null,
-  amount double precision not null,
-  "Remark" VARCHAR (355)
-);
--- tests/sql_functions/test_connection.py
+++ tests/sql_functions/test_connection.py
@@ -5,10 +5,10 @@ from aito.utils.sql_connection import SQLConnection
 from tests.test_case import TestCaseCompare
 
 
-class TestSQLConnection(TestCaseCompare):
+class TestPostgresConnection(TestCaseCompare):
     @classmethod
     def setUpClass(cls):
-        super().setUpClass(test_path='sql_functions/sql_connector')
+        super().setUpClass(test_path='sql_functions/connection')
         cls.input_folder = cls.input_folder.parent.parent / 'sample_invoice'
         env_variables = os.environ
         cls.connection = SQLConnection(
@@ -18,15 +18,24 @@ class TestSQLConnection(TestCaseCompare):
         c.close()
 
     def test_create_table(self):
-        with (self.input_folder / 'create_table.sql').open() as f:
-            query = f.read()
-        cursor = self.connection.execute_query(query)
+        cursor = self.connection.execute_query("""
+        CREATE TABLE invoice(
+            id serial primary key,
+            name VARCHAR(355) not null,
+            amount double precision not null,
+            \"Remark\" VARCHAR (355)
+        );
+        """)
         cursor.close()
 
     def test_insert_into_table(self):
-        with (self.input_folder / 'insert_into_table.sql').open() as f:
-            query = f.read()
-        cursor = self.connection.execute_query(query)
+        cursor = self.connection.execute_query("""
+        INSERT INTO invoice(name, amount, \"Remark\") VALUES 
+            ('Johnson, Smith, and Jones Co.', 345.33, 'Pays on time'),
+            (E'Sam \"Mad Dog\" Smith', 993.44, NULL),
+            ('Barney & Company', 0, E'Great to work with\nand always pays with cash.'),
+            (E'Johnson''s Automotive', 2344, NULL);
+        """)
         cursor.close()
 
     def test_query_all(self):
@@ -40,6 +49,56 @@ class TestSQLConnection(TestCaseCompare):
             results_as_json.append(row_as_json)
 
         with (self.input_folder / 'invoice.json').open() as f:
+            expected_results = json.load(f)
+        self.assertCountEqual(results_as_json, expected_results)
+        cursor.close()
+
+
+class TestMySQLConnection(TestCaseCompare):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass(test_path='sql_functions/connection')
+        cls.input_folder = cls.input_folder.parent.parent / 'sample_invoice'
+        env_variables = os.environ
+        cls.connection = SQLConnection(
+            env_variables.get('DRIVER'), env_variables.get('SERVER'), env_variables.get('PORT'),
+            env_variables.get('DATABASE'), env_variables.get('USER'), env_variables.get('PWD'))
+        c = cls.connection.execute_query('DROP TABLE IF EXISTS invoice;')
+        c.close()
+
+    def test_create_table(self):
+        cursor = self.connection.execute_query("""
+        CREATE TABLE invoice(
+            id serial primary key,
+            name VARCHAR(355) not null,
+            amount double precision not null,
+            remark VARCHAR (355)
+        );
+        """)
+        cursor.close()
+
+    def test_insert_into_table(self):
+        cursor = self.connection.execute_query("""
+        INSERT INTO invoice(name, amount, remark) VALUES
+          ('Johnson, Smith, and Jones Co.', 345.33, 'Pays on time'),
+          ('Sam \"Mad Dog\" Smith', 993.44, NULL),
+          ('Barney & Company', 0, 'Great to work with\nand always pays with cash.'),
+          ('Johnson''s Automotive', 2344, NULL);
+        """)
+        cursor.close()
+
+    def test_query_all(self):
+        cursor = self.connection.execute_query('SELECT * FROM invoice;')
+        col_names = [desc[0] for desc in cursor.description]
+        results_as_json = []
+        for row in cursor.fetchall():
+            row_as_json = {}
+            for idx, cell in enumerate(row):
+                row_as_json[col_names[idx]] = cell
+            results_as_json.append(row_as_json)
+
+        # MySQL column name not case sensitive
+        with (self.input_folder / 'invoice_lower_case_columns.json').open() as f:
             expected_results = json.load(f)
         self.assertCountEqual(results_as_json, expected_results)
         cursor.close()
