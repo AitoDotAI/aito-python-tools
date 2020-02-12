0e3e68fa1466df353dbe3ced40f687523db74ba1
Tri Nguyen <tringuyenminh23@gmail.com>
2020-02-12 15:43:29 +0200

    improve test infra

--- tests/test_result.py
+++ tests/test_result.py
@@ -1,20 +1,51 @@
-import os
 import unittest
+import subprocess
+from pathlib import Path
+import os
 
 
-class TestResultCompareFileMeld(unittest.TextTestResult):
+class TestResultLogMetrics(unittest.TextTestResult):
+    def __init__(self, stream, descriptions, verbosity):
+        super().__init__(stream, descriptions, verbosity)
+        self.metrics_log_path = Path(os.environ['METRICS_LOG_PATH']) if os.environ.get('METRICS_LOG_PATH') else None
+
+    def addSuccess(self, test):
+        super().addSuccess(test)
+        if self.metrics_log_path:
+            with self.metrics_log_path.open(mode='a+') as f:
+                f.write(f"{getattr(test, '_started_at', '')}, {test.id()}, success, {getattr(test, '_elapsed', '')},\n")
+
     def addFailure(self, test, err):
         super().addFailure(test, err)
-        if hasattr(test, 'out_file') and hasattr(test, 'exp_file'):
-            method_id = test.id().split('.')[-1]
-            if method_id in test.out_file and method_id in test.exp_file:
+        if self.metrics_log_path:
+            with self.metrics_log_path.open(mode='a+') as f:
+                f.write(f"{getattr(test, '_started_at', '')}, {test.id()}, fail, {getattr(test, '_elapsed', '')}, "
+                        f"{err}\n")
+
+    def addError(self, test, err):
+        super().addError(test, err)
+        if self.metrics_log_path:
+            with self.metrics_log_path.open(mode='a+') as f:
+                f.write(f"{getattr(test, '_started_at', '')}, {test.id()}, error, {getattr(test, '_elapsed', '')}, "
+                        f"{err}\n")
+
+
+class TestResultCompareFileMeld(TestResultLogMetrics):
+    def addFailure(self, test, err):
+        super().addFailure(test, err)
+        out_file_path = getattr(test, 'out_file_path')
+        exp_file_path = getattr(test, 'exp_file_path')
+        if out_file_path and exp_file_path:
+            if Path(out_file_path).exists() and Path(exp_file_path).exists():
                 cont = True
                 while cont:
                     res = input("[d]iff, [c]ontinue, or [f]reeze? ")
                     if res == "f":
-                        os.rename(test.out_file[method_id], test.exp_file[method_id])
+                        os.rename(out_file_path, exp_file_path)
                         cont = False
                     elif res == "c":
                         cont = False
                     elif res == "d":
-                        os.system("meld " + str(test.exp_file[method_id]) + " " + str(test.out_file[method_id]))
+                        subprocess.run(['meld', str(out_file_path), str(exp_file_path)])
+                    else:
+                        print('must input d, c, or f')
